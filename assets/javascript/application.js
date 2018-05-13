fc224a40a23b08b165b7d4ecda525c7cfbdf511c
Matthew Cibulka <mcibulkadev@gmail.com>
2018-05-13 13:49:15 -0400

    Dynamically add buttons with search topics to page, submit GET request to GIPHY API and display response to console.

--- assets/javascript/application.js
+++ assets/javascript/application.js
@@ -1,3 +1,38 @@
 $( document ).ready( function() {
-    console.log( "ready!" );
+    var topics = [ "dog", "panda", "confused", "the office", "smiling" ];
+
+    var GIPHY_API_URL = "http://api.giphy.com/v1/gifs/search";
+    var API_KEY = "api_key=pPsvHWkzBPkDPeTb5e0JIZdBoPBGEikZ";
+    var LIMIT = "limit=10";
+    var queryString = "?";
+
+    var q = "q=";       // search query term / phrase
+    var queryURL = "";
+
+
+    for( var i = 0 ; i < topics.length ; i++ ){
+        var b = $( "<button type='button' class='btn btn-info topic-btn'>" );
+        b.attr( "data-topic", topics[i] );
+        b.text( topics[i] );
+        $( "#btns-col" ).append( b );
+    }
+
+
+    $( ".topic-btn" ).on( "click", function () {
+        q += $(this).attr( "data-topic" );
+
+        queryString += API_KEY;
+        queryString += "&" + q;
+        queryString += "&" + LIMIT;
+
+        queryURL = GIPHY_API_URL + queryString;
+
+        $.ajax({
+            url: queryURL,
+            method: "GET"
+        })
+        .then( function( response ) {
+            console.log(response);
+        });
+    });
 });
