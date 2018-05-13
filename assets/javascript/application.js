d9d0fadf78f122c4779602c3f707e3c98d411ca4
Matthew Cibulka <mcibulkadev@gmail.com>
2018-05-13 18:33:10 -0400

    Get 10 GIFs from GIPHY based on button pressed and display still image to page with rating.

--- assets/javascript/application.js
+++ assets/javascript/application.js
@@ -11,15 +11,15 @@ $( document ).ready( function() {
 
 
     for( var i = 0 ; i < topics.length ; i++ ){
-        var b = $( "<button type='button' class='btn btn-info topic-btn'>" );
-        b.attr( "data-topic", topics[i] );
-        b.text( topics[i] );
-        $( "#btns-col" ).append( b );
+        var $b = $( "<button type='button' class='btn btn-info topic-btn'>" );
+        $b.attr( "data-topic", topics[i] );
+        $b.text( topics[i] );
+        $( "#btns-col" ).append( $b );
     }
 
 
-    $( ".topic-btn" ).on( "click", function () {
-        q += $(this).attr( "data-topic" );
+    $( ".topic-btn" ).on( "click", function() {
+        q += $( this ).attr( "data-topic" );
 
         queryString += API_KEY;
         queryString += "&" + q;
@@ -31,8 +31,22 @@ $( document ).ready( function() {
             url: queryURL,
             method: "GET"
         })
-        .then( function( response ) {
-            console.log(response);
+        .then( function( response ){
+            var data = response.data;   // array of GIFs matching search criteria 
+            console.log(data[0]);
+
+            $( "#gifs-row" ).empty();
+
+            for( var i = 0 ; i < data.length ; i++ ){
+                var $gifCol = $( "<div class='d-flex p-2 justify-content-center col'>");
+                var $gifDiv = $( "<div class='d-flex flex-column'>");
+
+                $gifDiv.append( $( "<h5 class='gif-rating'>" ).text( "Rating: " + data[i].rating.toUpperCase() ) );
+                $gifDiv.append( $( "<img class='gif-img'>" ).attr( "src", data[i].images.fixed_height_still.url ) );
+
+                $gifCol.append( $gifDiv );
+                $( "#gifs-row").append( $gifCol ); 
+            }
         });
     });
 });
